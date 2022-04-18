ec93a3afa0a1610340cc129b424de420d393571c
Guyls <101803405+Guyls@users.noreply.github.com>
2022-04-19 00:56:30 +0200

    Add files via upload

---
+++ practice2v/src/sync/TaskThread.java
@@ -0,0 +1,22 @@
+package sync;
+
+public class TaskThread extends Thread{
+
+    String CurrentThread;
+    int currentID;
+    TaskManager TaskManager;
+
+    public TaskThread(String CurrentThread, int currentID, TaskManager TaskManager) {
+        this.CurrentThread = CurrentThread;
+        this.currentID = currentID;
+        this.TaskManager = TaskManager;
+    }
+
+    @Override
+    public void run() {
+        TaskManager.performTask(CurrentThread, currentID);
+    }
+
+
+
+}
